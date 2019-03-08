da72734573115d20c99a32c8afdeb3cfcd9f8497
flavioespinoza <flavio.espinoza@gmail.com>
2019-03-08 06:52:50 -0700

    pre game for log_log

--- math.js
+++ math.js
@@ -1,33 +1,60 @@
-import { _error } from './error'
+import assert from 'assert'
+import _ from 'lodash'
+import { _log, log } from '@flavioespinoza/log_log'
+import { Chance } from 'chance'
 
-export const _divide = (a, b) => {
-    try {
-        return a / b
-    } catch (err) {
-        console.log('_divide', err)
+const chance = new Chance()
+
+const assertErrorInfo = err => {
+    return {
+        name: err.name,
+        code: err.code,
+        actual: err.actual,
+        expected: err.expected,
+        operator: err.operator
     }
 }
 
-export const _multiply = (a, b) => {
+const _deepStrictEqual = (a, b) => {
     try {
-        return a * b
+        assert.deepStrictEqual(a, b)
+        return {
+            success: true,
+            message: `assert.deepStrictEqual(${a}, ${b}) => true`,
+            info: {}
+        }
     } catch (err) {
-        _error('_multiply', err)
+        return {
+            success: false,
+            message: `assert.deepStrictEqual(${a}, ${b}) => false`,
+            info: assertErrorInfo(err)
+        }
     }
 }
 
-export const _add = (a, b) => {
+const _check = (a, b) => {
     try {
-        return a + b
+        assert.deepEqual(a, b)
+        return {
+            success: true,
+            message: `assert.deepEqual(${a}, ${b}) => true`,
+            info: {}
+        }
     } catch (err) {
-        _error('_add', err)
+        return {
+            success: false,
+            message: `assert.deepEqual(${a}, ${b}) => false`,
+            info: assertErrorInfo(err)
+        }
     }
 }
 
-export const _subtract = (a, b) => {
-    try {
-        return a - b
-    } catch (err) {
-        _error('_subtract', err)
-    }
-}
+let str1 = 'Hello'
+let str2 = chance.first()
+_log.info(str1)
+_log.warn(str2)
+console.log(_check(str1, str1))
+console.log(_deepStrictEqual(str1, str1))
+
+export { _check }
+export default _deepStrictEqual
